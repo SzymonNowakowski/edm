8ecfefd2805274d4a5b568c1f8b48cc763c31f98
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2025-08-09 12:25:19 +0200

    Getting rid of incompatible imports

--- torch_utils/git_commit_hash.py
+++ torch_utils/git_commit_hash.py
@@ -1,7 +1,6 @@
-from pathlib import Path
-
-def get_git_commit_hash_from_marker(marker_path: str | Path = "__COMMIT.txt") -> str:
-    for line in Path(marker_path).read_text().splitlines():
-        if line.startswith("commit:"):
-            return line.split("commit:", 1)[1].strip()
+def get_git_commit_hash_from_marker(marker_path="__COMMIT.txt"):
+    with open(marker_path, "r") as f:
+        for line in f:
+            if line.startswith("commit:"):
+                return line.split(":", 1)[1].strip()
     raise ValueError("No 'commit:' line found in marker file")
