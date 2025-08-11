6771638d25baecb140321cadd57711f672a41804
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2025-08-11 17:08:28 +0200

    updated script for lsf automated running

--- bsub_run.sh
+++ bsub_run.sh
@@ -1,2 +1,8 @@
-f="$1"
-bsub -J "$(basename "$f" .lsf)" < "$f"
+#!/bin/bash
+# bsub_auto.sh
+fname="$1"
+jobname="${fname%.lsf}"
+bsub -J "$jobname" \
+     -o "${jobname}_%J.out" \
+     -e "${jobname}_%J.err" \
+     < "$fname"
