73dc4fbeccc6fe06057aa0275690e3107fb43d9f
Szymon Nowakowski <szymon.nowakowski@gmail.com>
2025-10-24 19:24:20 +0200

    adding more printouts

--- generate.py
+++ generate.py
@@ -73,6 +73,7 @@ def edm_sampler(
         S_t_M = F_parametrization_S_t * torch.sqrt(M_const)
         lambda_prime = (ring_lambda_prime / (S_t_M + torch.sqrt((S_t_M * S_t_M - ring_lambda_prime).clamp_min(0)))) ** 2  ### numerically more stable, but equivalent to the original formula (S_t_M - torch.sqrt(S_t_M ** 2 - ring_lambda_prime)) ** 2
         # now we integrate numerically lambda_prime to get lambda with initial condition lambda(t0) = 0
+        print("The lambda prime values before integration:", lambda_prime.detach().cpu().numpy())
 
         t_starting_points = t_steps[:-1]
         t_ending_points = t_steps[1:]
@@ -97,10 +98,14 @@ def edm_sampler(
         ring_rho_inv = ring_rho_inv[subsample]
         r_t_inv = r_t_inv[subsample]
         rho_t = rho_t[subsample]
+        lambda_t = lambda_t[subsample]
+        lambda_prime = lambda_prime[subsample]
 
         print("The sigma schedule:", ring_rho_inv.detach().cpu().numpy())
         print("The r^-1 values:", r_t_inv.detach().cpu().numpy())
         print("The rho values:", rho_t.detach().cpu().numpy())
+        print("The lambda values:", lambda_t.detach().cpu().numpy())
+        print("The lambda prime values:", lambda_prime.detach().cpu().numpy())
 
         # Append an explicit final step (sigma=0) for convenience and recast to desired dtype (float32 by default)
         ring_rho_inv = torch.cat([ring_rho_inv, torch.zeros_like(ring_rho_inv[:1])]).to(dtype)  # sigma_N = 0
